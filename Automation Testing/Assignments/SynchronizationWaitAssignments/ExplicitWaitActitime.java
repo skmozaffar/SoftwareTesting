b1e0663dfb89db73c05afb03c45ded21d77df67e
Sk Mozaffar Hossain <100591545+skmozaffar@users.noreply.github.com>
2023-05-06 23:27:22 +0530

    Add files via upload

---
+++ Automation Testing/Assignments/SynchronizationWaitAssignments/ExplicitWaitActitime.java
@@ -0,0 +1,31 @@
+package SynchronizationWaitAssignments;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class ExplicitWaitActitime {
+	static {
+		System.setProperty("webdriver.chrome.driver","./driver/chromedriver.exe");
+	}
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://demo.actitime.com/login.do");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.findElement(By.xpath("//input[@type='text']")).sendKeys("admin");
+		driver.findElement(By.xpath("//input[@type='password']")).sendKeys("manager");
+		driver.findElement(By.id("loginButton")).click();
+		// Explicit Wait
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		wait.until(ExpectedConditions.titleContains("Enter"));
+		String title = driver.getTitle();
+		System.out.println(title);
+		driver.findElement(By.className("logout")).click();
+		driver.close();
+	}
+}
---
+++ Automation Testing/Assignments/SynchronizationWaitAssignments/ThreadSleepActitime.java
@@ -0,0 +1,22 @@
+package SynchronizationWaitAssignments;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ThreadSleepActitime {
+	static {
+		System.setProperty("webdriver.chrome.driver","./driver/chromedriver.exe");
+	}
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://demo.actitime.com/login.do");
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath("//input[@type='text']")).sendKeys("admin");
+		driver.findElement(By.xpath("//input[@type='password']")).sendKeys("manager");
+		driver.findElement(By.id("loginButton")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.className("logout")).click();
+		driver.close();
+	}
+}
