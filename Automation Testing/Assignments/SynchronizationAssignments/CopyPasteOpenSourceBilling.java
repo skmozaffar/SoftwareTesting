859a9c833e7b0937a55d0668ddcb887a521ed7e1
Sk Mozaffar Hossain <100591545+skmozaffar@users.noreply.github.com>
2023-05-16 21:09:47 +0530

    Add files via upload

---
+++ Automation Testing/Assignments/SynchronizationAssignments/CopyPasteOpenSourceBilling.java
@@ -0,0 +1,19 @@
+package SynchronizationAssignments;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CopyPasteOpenSourceBilling {
+	static {
+		System.setProperty("webdriver.chrome.driver","./driver/chromedriver.exe");
+	}
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://demo.opensourcebilling.org/en/users/sign_in");
+		driver.manage().window().maximize();
+		driver.findElement(By.id("email")).sendKeys(Keys.CONTROL + "ac");
+		driver.findElement(By.id("password")).sendKeys(Keys.CONTROL + "av");
+	}
+}
